00cb506396fb13423aa7fd1a188bef71d620c1fb
justanothermanoj <justanothermanoj@gmail.com>
2016-09-19 21:36:49 +0530

    Added play and pause buttons

--- scripts/components/react-video-player.js
+++ scripts/components/react-video-player.js
@@ -3,13 +3,26 @@ import React, {Component} from 'react'; // importing react from react library
 
 // main video components
 export default class ReactVideoPlayer extends Component {
+  constructor(props) {
+    super(props);
+    this.play = this.play.bind(this);
+    this.pause = this.pause.bind(this);
+  }
+  play() {
+    this.refs.player.play();
+  }
+  pause() {
+    this.refs.player.pause();
+  }
   render() {
     return (
       <div>
-          <video width={this.props.width} height={this.props.height} autoPlay={this.props.autoplay} loop={this.props.loop}>
+          <video ref="player" width={this.props.width} height={this.props.height} autoPlay={this.props.autoplay} loop={this.props.loop}>
             <source src="./videos/sample.mp4" type="video/mp4"></source>
             Your browser does not support HTML5 video.
           </video>
+          <button onClick={this.play}>Play</button>
+          <button onClick={this.pause}>Pause</button>
       </div>
     );
   }
