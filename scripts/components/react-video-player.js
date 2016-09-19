aa793d488e12ad771686a9f9f1f58c2b28816cbf
manoj <manoj.techindustan@gmail.com>
2016-09-19 13:32:25 +0530

    removed default controls

--- scripts/components/react-video-player.js
+++ scripts/components/react-video-player.js
@@ -6,7 +6,7 @@ export default class ReactVideoPlayer extends Component {
   render() {
     return (
       <div>
-          <video width="400" controls>
+          <video width="400">
             <source src="./videos/sample.mp4" type="video/mp4"></source>
             Your browser does not support HTML5 video.
           </video>
